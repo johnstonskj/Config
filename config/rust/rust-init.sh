87bdcbc3a3a0b3d0a8f9619a9e563913ac8e8f07
Simon Johnston <johnstonskj@gmail.com>
2025-11-13 19:11:22 -0800

    feat: moved brewfile in, fixed up rust

---
+++ config/rust/rust-init.sh
@@ -0,0 +1,32 @@
+# -*- mode: sh; eval: (sh-set-shell "bash") -*-
+#!/usr/bin/env bash
+
+declare -a TOOLCHAINS=(
+	stable
+)
+
+declare -a COMPONENTS=(
+	rust-src
+	clippy-preview
+)
+
+declare -a COMMANDS=(
+	cargo-info
+	cargo-tarpaulin
+	cargo-update
+	cargo-watch
+	nu
+	nu-lint
+)
+
+for toolchain in "${TOOLCHAINS[@]}"; do
+	rustup toolchain add "${toolchain}"
+done
+
+for component in "${COMPONENTS[@]}"; do
+	rustup component add "${component}"
+done
+
+for command in "${COMMANDS[@]}"; do
+	cargo install --locked "${command}"
+done
